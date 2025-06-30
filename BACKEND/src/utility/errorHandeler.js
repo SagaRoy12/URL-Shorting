e6e3c7075b3288bfc4a987d003988f65a22a3e74
Sagar <sagarroy20027@gmail.com>
2025-06-30 23:53:44 +0530

    added error handeler for different error cases using class interface

---
+++ BACKEND/src/utility/errorHandeler.js
@@ -0,0 +1,48 @@
+import { request , response , NextFunction } from "express";
+
+
+export const errorHandeler =(err, req = request, res = response, next = NextFunction)=>{
+console.log(`the error is ${err}`)  // testing error 
+    if(err instanceof  AppError){
+        return res.status(err.statusCode).json({
+            success: false,
+            message: err.message
+        })
+    }
+        res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        })
+}
+
+export class AppError extends Error{
+    statusCode; isOperatioinal;
+    constructor(message , statusCode = 500 , isOperatioinal= true){
+        super(message);
+        this.statusCode = statusCode;
+        this.isOperatioinal = isOperatioinal;
+        Error.captureStackTrace (this , this.constructor);
+    }
+}
+
+export class NotFoundError extends AppError{
+    constructor(message = `resource not found`){
+        super (message, 404)
+    }
+}
+export class ConflictError extends AppError{
+    constructor(message = `conflict occured`){
+        super (message, 409)
+    }
+}
+export class BadRequestError extends AppError{
+    constructor(message = `bad request`){
+        super (message, 400)
+    }
+}
+export class UnauthorizedError extends AppError{
+    constructor(message = `unauthorized`){
+        super (message, 401)
+    }
+}
+
