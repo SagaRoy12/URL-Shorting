9f8cc49f4284e2ad10d7323d39d04b2f17c5cd9a
Sagar <sagarroy20027@gmail.com>
2025-06-30 23:54:36 +0530

    added error handeler

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv'
 import { redirectFromShortUrl } from './src/controller/shortUrl_Controller.js' 
 import ShortUrl from './src/dbSchema/short.url.schema.js'
 dotenv.config("./.env") // Load environment variables from .env file
-
+import {errorHandeler} from './src/utility/errorHandeler.js'
 import connectDB from './src/config/mongo.config.js' // Importing the MongoDB connection function
 import short_urlRoute from './src/routes/shortUrl_Routes.js' // Importing the short URL routes
 app.use(express.json())
@@ -15,6 +15,7 @@ app.use("/api/create" , short_urlRoute);
 
 app.get("/:shortenedUrl", redirectFromShortUrl) 
 
+app.use(errorHandeler)
 connectDB().then(() => {
     app.listen(port, () => {
         console.log(`Server is running at http://localhost:${port}`);
