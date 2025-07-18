4db06e894c53abcdd530589314650b8d127df252
Sagar <sagarroy20027@gmail.com>
2025-07-18 23:34:26 +0530

    added authentication middleware

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000 
 import dotenv from 'dotenv'
 import { redirectFromShortUrl } from './src/controller/shortUrl_Controller.js' 
-import ShortUrl from './src/dbSchema/short.url.schema.js'
+import auth_route from './src/routes/authentication_Route.js'
 dotenv.config("./.env") // Load environment variables from .env file
 import {errorHandeler} from './src/utility/errorHandeler.js'
 import connectDB from './src/config/mongo.config.js' // Importing the MongoDB connection function
@@ -15,6 +15,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: true })) // for parsing th URL-encoded data
+
+app.use("/api/auth" , auth_route);
 
 app.use("/api/create" , short_urlRoute);
 
