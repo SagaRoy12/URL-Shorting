a40638d2d6ba6b1cc6594e9497503bfb5bf98250
Sagar <sagarroy20027@gmail.com>
2025-07-18 23:25:55 +0530

    added the comment what it actually used for

--- BACKEND/src/dataAccessObject/shortUrl.dao.js
+++ BACKEND/src/dataAccessObject/shortUrl.dao.js
@@ -1,11 +1,17 @@
 import ShortUrl from "../dbSchema/short.url.schema.js" // Importing the short URL schema
 
+// shortUrl dao helps to save the short url and long url in database    
 
 export const shortUrlSaver = async (generatedShortUrl , longUrl , userId)=>{
+    
     const newShortUrl = new ShortUrl({  // instance to be saved in databse
+    
         full_url: longUrl, // Full URL to be shortened
+      
         short_url: generatedShortUrl, // Generating a unique short URL using nanoid
+      
         clicks: 0 // Initializing clicks to 0
+        
     })
     if (userId) {
         newShortUrl.user = userId // If a user ID is provided, associate it with the short URL
