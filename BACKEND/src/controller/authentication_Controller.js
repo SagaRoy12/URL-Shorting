3a6ba4ad0c1fbefb8bc83aac03b792357183cb67
Sagar <sagarroy20027@gmail.com>
2025-08-03 22:20:25 +0530

    updated the controller for the authentication

--- BACKEND/src/controller/authentication_Controller.js
+++ BACKEND/src/controller/authentication_Controller.js
@@ -1,13 +1,18 @@
 import {registerUserService} from "../services/authentication_Service.js"
 import tryCatchWrapperForErrorHandeling from "../utility/tryCatchWrapper.js"
+import {cookieOptions} from "../config/cookieOptions.config.js"
 
 export const registerUser =tryCatchWrapperForErrorHandeling( async (req, res)=>{
 
     const{name , email, password} = req.body
 
     const token = await registerUserService(name , email , password)
-    console.log(token)
-    res.status(200).json(token)
+
+    //console.log(token)
+   
+    res.cookie("ACCES_TOKEN", token , cookieOptions)  // saving the cookie with the token and options
+
+    res.status(200).json({message:"user registered"})
 })
 
 export const loginUser = async (req, res)=>{
