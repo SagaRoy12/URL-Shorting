a8ccc1cdeb6cc8ab3ee04e720c0f1c3bf1172a3e
Sagar <sagarroy20027@gmail.com>
2025-12-30 16:50:19 +0530

    added cookie parser

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,18 +1,25 @@
 import express from 'express'
 const app = express()
-const port = 3000 
+const port = 3000
 import dotenv from 'dotenv'
-import { redirectFromShortUrl } from './src/controller/shortUrl_Controller.js' 
+import { redirectFromShortUrl } from './src/controller/shortUrl_Controller.js'
 import auth_route from './src/routes/authentication_Route.js'
-dotenv.config("./.env") // Load environment variables from .env file
 import {errorHandeler} from './src/utility/errorHandeler.js'
 import connectDB from './src/config/mongo.config.js' // Importing the MongoDB connection function
 import short_urlRoute from './src/routes/shortUrl_Routes.js' // Importing the short URL routes
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import { attachUser } from './src/utility/attachUser.helper.js'
+dotenv.config("./.env") // Load environment variables from .env file
+app.use(cors({
+    origin: 'http://localhost:5173', // Your frontend URL
+    credentials: true // Allow cookies to be sent
+}))
 
-app.use(cors())
+app.use(cookieParser())
 
 app.use(express.json())
+app.use(attachUser)
 
 app.use(express.urlencoded({ extended: true })) // for parsing th URL-encoded data
 
