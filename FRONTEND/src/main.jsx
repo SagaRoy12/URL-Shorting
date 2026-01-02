01df1897a910be0e93c28690fdb3e27b7d92634f
Sagar <sagarroy20027@gmail.com>
2026-01-02 12:05:14 +0530

    added Tanstack Router Provider

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -2,16 +2,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { routeTree } from './routing/routeTree'
 
 const queryClient = new QueryClient()
-
+const router = createRouter({ routeTree })
 createRoot(document.getElementById('root')).render(
-  
+
   <QueryClientProvider client={queryClient}>
-  
-           <App />
-  
+    <RouterProvider router={router} />
+
+
   </QueryClientProvider>
-    
-  
+
+
 )
