a6bd14640bfe953cfcf89b52977bae89665ed1fc
Sagar <sagarroy20027@gmail.com>
2026-01-03 01:27:53 +0530

    added bycrypt and convert the userSchema to json and hide the password from the response

--- BACKEND/src/dbSchema/user.schema.js
+++ BACKEND/src/dbSchema/user.schema.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true  
+        required: true
     },
     email: {
         type: String,
@@ -19,12 +20,28 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-// function too generate avatar with the unique hash 
-function generateAvatar(email){
-   
-    const hash = require('crypto').createHash('md5').update(email).digest('hex');
-    return `https://www.gravatar.com/avatar/${hash}?d=mp`
-}
 
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+// Hashing the Password before saving it to the database
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next()
+    } else {
+        this.password = await bcrypt.hash(this.password, 10)  // hashing using bycript
+        next();
+    }
+})
+
+
+// hiding the password anf __v from the response by converting the object to json and applying delete operation in it
+userSchema.set(`toJSON`, {
+    transform: function (doc, ret) {
+        delete ret.password
+        delete ret.__v
+        return ret
+    }
+})
 const User = mongoose.model('User', userSchema);
 export default User;
