96210924a1b697da4de8c908c9fa0da43e79a09c
Sagar <sagarroy20027@gmail.com>
2026-01-30 00:44:58 +0530

    added log statement for log

--- BACKEND/src/jwt/jsonWebTokenValidation.jwt.js
+++ BACKEND/src/jwt/jsonWebTokenValidation.jwt.js
@@ -2,6 +2,6 @@ import jsonwebtoken from 'jsonwebtoken'
 
 export const verifyJsonWebToken = async (token )=>{
    const decodedToken =  await jsonwebtoken.verify(token , process.env.JWT_SECRET )
-   console.log(decodedToken.id)
+   console.log("decodedToken is ------:", decodedToken.id);
    return decodedToken.id
 }
